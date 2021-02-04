7c34132b331f7dfebde0f7d0fbd03b227d893758
serraguti <pacoserranox@gmail.com>
2021-02-04 09:36:56 +0100

    commit

---
+++ MvcCore/Controllers/EmpleadosSessionController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCore.Models;
+using MvcCore.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCore.Controllers
+{
+    public class EmpleadosSessionController : Controller
+    {
+        IRepositoryHospital repo;
+
+        public EmpleadosSessionController(IRepositoryHospital repo) {
+            this.repo = repo;
+        }
+
+        public IActionResult AlmacenarEmpleados()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+    }
+}
